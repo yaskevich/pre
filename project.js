20722ac4f0af6a9927bc37d062493534018e83c8
yaskevich <alyaxey.yaskevich@gmail.com>
2020-04-04 21:16:50 +0300

    base64 image

--- project.js
+++ project.js
@@ -1,8 +1,18 @@
+var data, texts;
 $(function() {
     var lang = "en";
-    var strings = {};
-	var tab = "";
-	toggleLanguage(lang);
+    var locale = {};
+    var tab = "";
+
+	if (!data || !texts) {
+		console.log("get JSONs");
+		$.ajaxSetup( { "async": false } );
+		data = $.getJSON("nirs.json").responseJSON;
+		texts = $.getJSON("texts.json").responseJSON;
+		$.ajaxSetup( { "async": true } );
+	}
+
+    toggleLanguage(lang);
 	
     $(document).on("click", ".langswitch", function() {
         lang = (lang == "en" ? "ru" : "en");
@@ -10,71 +20,64 @@ $(function() {
     }).on("click", ".downloadbtn", function() {
         download($(this).val() === "CSV");
     }).on("click", ".tabs", function() {
-		toggleTabs($(this))
+        toggleTabs($(this))
     });
-	
-	function toggleTabs(that){
-		var id = that.attr('id');
-		if(id !== tab) {
-			that.css('background-color', '#f8f8f8');
-			that.css('border', '1px dashed lightgray');
-			if (tab) {
-				$('#'+tab).css('background-color', '');
-				$('#'+tab).css('border', '');
-			}
-			tab = id;
-			var divsToHide  = $('.data:not(.hidden)');
-			$('.data.hidden').removeClass('hidden');
-			divsToHide.addClass('hidden');
-		} 		
-	}
-	
-    function download(mode) {
-        $.getJSON("nirs.json", function(data) {
-            // console.log(data);
-            var todo = {
-                "NOUNS": $('#nouns').prop('checked'),
-                "VERBS": $('#verbs').prop('checked'),
-                "ADJECTIVES": $('#adjs').prop('checked'),
-                "ADVERBS": $('#advs').prop('checked')
-            }
-            // $('.example1').typeIt({
-            // whatToType: "The model is ready",
-            // typeSpeed: 50
-            // });
 
+
+    function toggleTabs(that) {
+        var id = that.attr('id');
+        if (id !== tab) {
+            that.css('background-color', '#f8f8f8');
+            that.css('border', '1px dashed lightgray');
+            if (tab) {
+                $('#' + tab).css('background-color', '');
+                $('#' + tab).css('border', '');
+            }
+            tab = id;
+            var divsToHide = $('.data:not(.hidden)');
+            $('.data.hidden').removeClass('hidden');
+            divsToHide.addClass('hidden');
+        }
+    }
+
+    function download(mode) {
+        var todo = {
+            "NOUNS": $('#nouns').prop('checked'),
+            "VERBS": $('#verbs').prop('checked'),
+            "ADJECTIVES": $('#adjs').prop('checked'),
+            "ADVERBS": $('#advs').prop('checked')
+        }
+        if (todo["NOUNS"] + todo["VERBS"] + todo["ADJECTIVES"] + todo["ADVERBS"]) {
             var datum = {};
-            var csv = mode?(strings["table"].join(',') + "\n"):null;
+            var csv = mode ? (locale["table"].join(',') + "\n") : null;
             var stops = [];
-			
-			if ($('#stops').prop('checked')){
-				var stopline = $('.stopline').val().replace(/\s/g, '');
-				if (stopline) {
-					stops = stopline.split(',');
-				}
-			}
-			
-			console.log(stops);
-			
-			
+
+            if ($('#stops').prop('checked')) {
+                var stopline = $('.stopline').val().replace(/\s/g, '');
+                if (stopline) {
+                    stops = stopline.split(',');
+                }
+            }
+
             for (var prop in data) {
                 var unit = prop.split('_');
                 if (todo[unit[1]]) {
                     if (mode) {
                         for (var el of data[prop]) {
-							if (stops.indexOf(el[0]) === -1){
-								csv += [unit[0], unit[1]].concat(el).join(',') + "\n";
-							}
+                            if (stops.indexOf(el[0]) === -1) {
+                                csv += [unit[0], unit[1]].concat(el).join(',') + "\n";
+                            }
                         }
                     } else {
-						datum[prop] = stops.length ? 
-							data[prop].filter(function(d){ return stops.indexOf(d[0]) ==-1 })
-							:
-							data[prop];
-						
-						// 
-					}
-                } 
+                        datum[prop] = stops.length ?
+                            data[prop].filter(function(d) {
+                                return stops.indexOf(d[0]) == -1
+                            }) :
+                            data[prop];
+
+                        // 
+                    }
+                }
             }
 
             var fn = "data." + (mode ? "csv" : "json");
@@ -83,21 +86,18 @@ $(function() {
                 name: fn
             });
             saveAs(file, fn);
-        });
-
+        }
     }
 
     function toggleLanguage(lang) {
-        $.getJSON("texts.json", function(data) {
-            strings = {};
-            for (var prop in data) {
-                strings[prop] = data[prop][lang];
-            }
-            $('body').html(Mustache.render($('#template').html(), strings));
-            $('title').text(strings["title"]);
-            $('html').attr("lang", lang);
-			tab = "";
-			toggleTabs($('#btn_desc'));
-        })
+        locale = {};
+        for (var prop in texts) {
+            locale[prop] = texts[prop][lang];
+        }
+        $('body').html(Mustache.render($('#template').html(), locale));
+        $('title').text(locale["title"]);
+        $('html').attr("lang", lang);
+        tab = "";
+        toggleTabs($('#btn_desc'));
     }
 });
